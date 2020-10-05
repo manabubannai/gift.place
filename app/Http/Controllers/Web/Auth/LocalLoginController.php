1bda52a6c5b717d8e9c4a00e1572409336a6a515
RikuHirose <rikuparkour9@gmail.com>
2020-10-05 19:51:32 +0900

    issue_#10/ローカル開発用のログイン

---
+++ app/Http/Controllers/Web/Auth/LocalLoginController.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Http\Controllers\Web\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\Image;
+use App\Models\User;
+use Auth;
+
+class LocalLoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/?status=success&message=ログインしました';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function getLocalLogin()
+    {
+        return view('auth.local-login');
+    }
+
+    public function postLocalRegister(Request $request)
+    {
+        \DB::beginTransaction();
+        try {
+
+            $user = User::create([
+                'name'             => $request->name,
+                'email'            => $request->email,
+            ]);
+
+            \DB::commit();
+        } catch (\Exception $e) {
+            \Log::error($e);
+            \DB::rollback();
+            return redirect('/local/login');
+        }
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,8 +21,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
 
-            $table->string('password');
-
             $table->rememberToken();
 
             $table->foreign('bg_file_id')->references('id')->on('files')->onDelete('cascade');
