19aa82278e31246338fe281c6cc1843b8e022f7f
RikuHirose <rikuparkour9@gmail.com>
2020-11-02 22:55:52 +0900

    add-testCardChange

--- tests/Feature/Controllers/Web/User/SubscriptionControllerTest.php
+++ tests/Feature/Controllers/Web/User/SubscriptionControllerTest.php
@@ -5,11 +5,11 @@ use Tests\TestCase;
 
 class SubscriptionControllerTest extends TestCase
 {
-    private const SUCCESS_PAYMENT_METHOD_VISA                        = 'pm_card_visa';
-    private const SUCCESS_PAYMENT_METHOD_MASTER                      = 'pm_card_mastercard';
-    private const PAYMENT_METHOD_カード情報不正                             = 'pm_card_cvcCheckFail';
-    private const PAYMENT_METHOD_決済失敗                                = 'pm_card_chargeDeclined';
-    private const PAYMENT_METHOD_カード情報正常_決済失敗                        = 'pm_card_chargeCustomerFail';
+    private const SUCCESS_PAYMENT_METHOD_VISA         = 'pm_card_visa';
+    private const SUCCESS_PAYMENT_METHOD_MASTER       = 'pm_card_mastercard';
+    private const PAYMENT_METHOD_カード情報不正              = 'pm_card_cvcCheckFail';
+    private const PAYMENT_METHOD_決済失敗                 = 'pm_card_chargeDeclined';
+    private const PAYMENT_METHOD_カード情報正常_決済失敗         = 'pm_card_chargeCustomerFail';
 
     public function setUp(): void
     {
@@ -29,8 +29,6 @@ class SubscriptionControllerTest extends TestCase
         ];
 
         $response = $this->actingAs($user)->post('/subscriptions', $data);
-
-        // dd($response);
 
         $response
             ->assertRedirect('/dashboard')
@@ -129,5 +127,48 @@ class SubscriptionControllerTest extends TestCase
                     'message' => '入村手続きが完了しました',
                 ],
             ]);
+    }
+
+    public function testCardChangeSuccess()
+    {
+        $user           = factory(\App\Models\User::class)->create();
+        $stripeCustomer = $user->createOrGetStripeCustomer();
+        $user->fill([
+            'stripe_id' => $stripeCustomer->id,
+        ])->save();
+        $user->updateDefaultPaymentMethod(self::SUCCESS_PAYMENT_METHOD_VISA);
+
+        $data  = [
+          'payment_method' => self::SUCCESS_PAYMENT_METHOD_MASTER,
+        ];
+
+        $response = $this->actingAs($user)->post('/subscriptions/card-change', $data);
+
+        $response
+            ->assertSessionHasAll([
+                'toast' => [
+                    'status'  => 'success',
+                    'message' => 'カードを変更しました',
+                ],
+            ]);
+    }
+
+    public function testCardChangeFail_カード情報不正()
+    {
+        $user           = factory(\App\Models\User::class)->create();
+        $stripeCustomer = $user->createOrGetStripeCustomer();
+        $user->fill([
+            'stripe_id' => $stripeCustomer->id,
+        ])->save();
+        $user->updateDefaultPaymentMethod(self::SUCCESS_PAYMENT_METHOD_VISA);
+
+        $data  = [
+          'payment_method' => self::PAYMENT_METHOD_カード情報不正,
+        ];
+
+        $response = $this->actingAs($user)->post('/subscriptions', $data);
+
+        $response->assertStatus(200);
+        $this->assertSame("Your card's security code is incorrect.", $response->getContent());
     }
 }
