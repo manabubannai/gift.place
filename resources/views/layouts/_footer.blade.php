5e1f47025360de9234fc1d5e788ee7f278a202d3
RikuHirose <rikuparkour9@gmail.com>
2021-04-16 14:11:06 +0900

    add-contact-link

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,7 +4,7 @@
       <li><a href="https://manablog.org/" class="s-footer_nav--link">運営会社</a></li>
       <li><a href="{{ route('term') }}" class="s-footer_nav--link">利用規約</a></li>
       <li><a href="{{ route('policy') }}" class="s-footer_nav--link">プライバシーポリシー</a></li>
-      <li><a href="{{ route('contact') }}" class="s-footer_nav--link">お問い合わせ</a></li>
+      <li><a href="https://twitter.com/messages/926630074010427392-1223403110023827457" class="s-footer_nav--link" target="__blank">お問い合わせ</a></li>
     </ul>
   </nav>
   <small>Copyright - Manabu Bannai, 2021 All Rights Reserved.</small>
